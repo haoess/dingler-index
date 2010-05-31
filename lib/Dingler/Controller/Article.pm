d34cb778738e3570ebab8c173fc83184a9421535
Frank Wiegand <frank.wiegand@gmail.com>
2010-05-31 12:15:38 +0200

    article navigation

--- lib/Dingler/Controller/Article.pm
+++ lib/Dingler/Controller/Article.pm
@@ -39,7 +39,7 @@ sub index :Path :Args(2) {
     $c->stash->{xsl} = $c->res->body;
     $c->res->body( undef );
 
-    # plain article
+    # build the plain article for the tag cloud
     $c->stash->{template} = 'article-plain.xsl';
     $c->forward('Dingler::View::XSLT');
     my $plain = $c->res->body;
@@ -52,11 +52,35 @@ sub index :Path :Args(2) {
     $c->stash( cloud => $cloud );
     $c->res->body( undef );
 
+    my $prev_article = $c->forward( 'step_article', ['preceding', $xml, $article] );
+    my $next_article = $c->forward( 'step_article', ['following', $xml, $article] );
+
     # page
     $c->stash(
-        template => 'article/view.tt',
+        prev_article => $prev_article,
+        next_article => $next_article,
+        template     => 'article/view.tt',
     )
 }
+
+sub step_article :Private {
+    my ($self, $c, $way, $xml, $article) = @_;
+
+    my $ns_uri = 'http://www.tei-c.org/ns/1.0';
+    my $parser = XML::LibXML->new();
+    $parser->line_numbers(1);
+    $parser->load_ext_dtd(0);
+    $parser->expand_entities(0);
+
+    my $doc = $parser->parse_file($xml) or die $!;
+    my $xpc = XML::LibXML::XPathContext->new( $doc ) or die $!;
+    $xpc->registerNs( 'tei', $ns_uri );
+
+    my $ret = $xpc->find( '//tei:text[@xml:id="' . $article . '"]/' . $way . '::tei:text[@type="art_undef" or @type="art_patent"][1]/@xml:id' );
+    return $ret;
+}
+
+sub next_article {}
 
 =head1 AUTHOR
 
