a84d06bed2f4c3d9493652782afc47c21d105e1b
Frank Wiegand <frank.wiegand@gmail.com>
2010-06-18 00:46:17 +0200

    add feedback button

---
+++ lib/Dingler/Controller/Bug.pm
@@ -0,0 +1,55 @@
+package Dingler::Controller::Bug;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Dingler::Controller::Bug - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched Dingler::Controller::Bug in Bug.');
+}
+
+=head2 report
+
+=cut
+
+sub report :Local {
+    my ( $self, $c, $article ) = @_;
+    my $bugtype = $c->req->params->{bugtype};
+    my $ocrword = $c->req->params->{ocrword};
+    my $email   = $c->req->params->{email};
+    my $note    = $c->req->params->{note};
+    $c->res->content_type('text/plain');
+    $c->res->body('Feedback erhalten.');
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+=head1 AUTHOR
+
+Frank Wiegand
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
