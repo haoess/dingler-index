405733cfb83f37907910f2d7290efb9fe54a56c5
Frank Wiegand <frank.wiegand@gmail.com>
2010-06-17 10:09:28 +0200

    update catalyst scripts

--- script/dingler_create.pl
+++ script/dingler_create.pl
@@ -25,17 +25,20 @@ dingler_create.pl [options] model|view|controller name [helper] [options]
    dingler_create.pl controller My::Controller
    dingler_create.pl -mechanize controller My::Controller
    dingler_create.pl view My::View
-   dingler_create.pl view MyView TT
-   dingler_create.pl view TT TT
+   dingler_create.pl view HTML TT
    dingler_create.pl model My::Model
    dingler_create.pl model SomeDB DBIC::Schema MyApp::Schema create=dynamic\
    dbi:SQLite:/tmp/my.db
    dingler_create.pl model AnotherDB DBIC::Schema MyApp::Schema create=static\
-   dbi:Pg:dbname=foo root 4321
+   [Loader opts like db_schema, naming] dbi:Pg:dbname=foo root 4321
+   [connect_info opts like quote_char, name_sep]
 
  See also:
    perldoc Catalyst::Manual
    perldoc Catalyst::Manual::Intro
+   perldoc Catalyst::Helper::Model::DBIC::Schema
+   perldoc Catalyst::Model::DBIC::Schema
+   perldoc Catalyst::View::TT
 
 =head1 DESCRIPTION
 
