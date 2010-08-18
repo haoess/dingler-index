f17bbe2c7b4b0d5aafb46dc8a7494f883aa2d8ff
Frank Wiegand <frank.wiegand@gmail.com>
2010-08-18 23:32:31 +0200

    add timeline example

---
+++ lib/Dingler/Controller/Timeline.pm
@@ -0,0 +1,43 @@
+package Dingler::Controller::Timeline;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Dingler::Controller::Timeline - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=head2 dta
+
+=cut
+
+sub dta :Local {
+    my ( $self, $c, $year ) = @_;
+    $c->stash(
+        template => 'timeline.tt',
+        year     => $year,
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+=head1 AUTHOR
+
+Frank Wiegand
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
