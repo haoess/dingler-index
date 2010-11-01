3cf51c5548d9d1da459a8afd7eb53544caa6dcf9
Frank Wiegand <frank.wiegand@gmail.com>
2010-11-01 22:27:41 +0100

    add PND-BEACON support

--- lib/Dingler/Controller/Person.pm
+++ lib/Dingler/Controller/Person.pm
@@ -28,11 +28,14 @@ sub index :Path :Args(2) {
     $c->stash->{xml} = $xml;
     $c->stash->{template} = 'person.xsl';
 
+    my $search_args = {
+        'me.id' => $person,
+    };
+    if ( $article ) {
+        $search_args->{ 'ref.id' } = { '!=' => $article };
+    }
     my $rs = $c->model('Dingler::Person')->search(
-        {
-            'me.id'      => $person,
-            'ref.id' => { '!=' => $article },
-        },
+        $search_args,
         {
             join => [ 'ref' ],
         },
@@ -101,6 +104,56 @@ sub list :Local {
     $c->stash(
         template => 'person/list.tt',
     );
+}
+
+=head2 beacon
+
+=cut
+
+sub beacon :Global {
+    my ( $self, $c ) = @_;
+    my $xml = XML::LibXML->new->parse_file( $c->config->{svn} . '/database/persons/persons.xml' );
+    my $xpc = XML::LibXML::XPathContext->new( $xml ) or die $!;
+    $xpc->registerNs( 'tei', 'http://www.tei-c.org/ns/1.0' );
+
+    my %pnds;
+    foreach my $person ( $xpc->findnodes('//tei:person') ) {
+        my $pnd = $xpc->find( 'tei:note[@type="pnd"]', $person );
+        next unless $pnd;
+        my $id = $xpc->find( '@xml:id', $person );
+        my $count = $c->model('Dingler::Person')->search({ id => $id })->count;
+        next unless $count;
+        $pnds{$pnd} = $count;
+    }
+    $c->res->content_type( 'text/plain' );
+    my $out = <<"EOT";
+#FORMAT: PND-BEACON
+#TARGET: @{[ $c->uri_for('/person/pnd') ]}/{ID}
+#VERSION: 0.1
+#FEED: @{[ $c->req->uri ]}
+#CONTACT: Frank Wiegand <frank.wiegand\@gmail.com>
+#INSTITUTION: Digitalisierung des Polytechnischen Journals (Institut fuer Kulturwissenschaft, Humboldt-Universitaet zu Berlin)
+EOT
+    $out .= join "\n", map { sprintf "%s|%d", $_, $pnds{$_} } sort keys %pnds;
+    $c->res->body( $out );
+}
+
+=head2 pnd
+
+=cut
+
+sub pnd :Local {
+    my ( $self, $c, $pnd ) = @_;
+    my $xml = XML::LibXML->new->parse_file( $c->config->{svn} . '/database/persons/persons.xml' );
+    my $xpc = XML::LibXML::XPathContext->new( $xml ) or die $!;
+    $xpc->registerNs( 'tei', 'http://www.tei-c.org/ns/1.0' );
+    my ( $entry ) = $xpc->findnodes("//tei:person[tei:note[\@type='pnd'] = '$pnd']");
+    if ( $entry ) {
+        my $id = $xpc->find( '@xml:id', $entry );
+        my $person = $c->model('Dingler::Person')->search({ person => $id });
+        $c->forward('index', [ $id, '' ]);
+        use Data::Dumper; warn Dumper $c->res->body;
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
