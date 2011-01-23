4ecb08c3d44cca487ddc362b3fa48fd099837b8e
Frank Wiegand <frank.wiegand@gmail.com>
2011-01-23 21:22:48 +0100

    parallelize cascade

--- bin/cascade.sh
+++ bin/cascade.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
+which parallel > /dev/null
+HAVE_PARALLEL="$?"
+
 echo "persons.xml to sql ..."
 bin/postgres_build_persons.pl
 
 echo "start basic database build ..."
-for i in "$@"; do
-  bin/postgres_build_index.pl "$i"
-done
+if [ $HAVE_PARALLEL -eq 0 ]; then
+  parallel -j-1 bin/postgres_build_index.pl ::: $@
+else
+  for i in "$@"; do
+    bin/postgres_build_index.pl "$i"
+  done
+fi
 
 echo "start patent database setup ..."
-for i in "$@"; do
-  bin/postgres_patents.pl "$i"
-done
+if [ $HAVE_PARALLEL -eq 0 ]; then
+  parallel -j-1 bin/postgres_patents.pl ::: $@
+else
+  for i in "$@"; do
+    bin/postgres_patents.pl "$i"
+  done
+fi
 
 echo "map person references ..."
-for i in "$@"; do
-  bin/postgres_build_personrefs.pl "$i"
-done
+if [ $HAVE_PARALLEL -eq 0 ]; then
+  parallel -j-1 bin/postgres_build_personrefs.pl ::: $@
+else
+  for i in "$@"; do
+    bin/postgres_build_personrefs.pl "$i"
+  done
+fi
 
 echo "rebuild sphinx index ..."
 indexer --config sphinx.conf --all
